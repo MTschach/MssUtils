56438509e808a4aa9662d7355f9d2c4685414d43
Michael Tschach <Michael.Tschach@xcom.de>
2018-12-27 10:28:00 +0100

    added Tests

--- src/main/java/de/mss/utils/db/DBResult.java
+++ src/main/java/de/mss/utils/db/DBResult.java
@@ -1,35 +1,41 @@
 package de.mss.utils.db;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 
 public class DBResult {
+
+   public static final String     AS_STRING_VALUE     = null;
+   public static final Integer    AS_INTEGER_VALUE    = null;
+   public static final Boolean    AS_BOOLEAN_VALUE    = null;
+   public static final BigDecimal AS_BIGDECIMAL_VALUE = null;
 
    ArrayList<DBSingleResult> resultList = null;
 
 
    public DBResult() {
-      resultList = new ArrayList<>();
+      this.resultList = new ArrayList<>();
    }
 
 
    public int getNumberOfResults() {
-      return resultList.size();
+      return this.resultList.size();
    }
 
 
    public int getColumnCount(int resultSetNumber) {
-      if (resultSetNumber < 0 || resultSetNumber >= resultList.size())
+      if (resultSetNumber < 0 || resultSetNumber >= this.resultList.size())
          return 0;
 
-      return resultList.get(resultSetNumber).getColumnCount();
+      return this.resultList.get(resultSetNumber).getColumnCount();
    }
 
 
    public int getRowCount(int resultSetNumber) {
-      if (resultSetNumber < 0 || resultSetNumber >= resultList.size())
+      if (resultSetNumber < 0 || resultSetNumber >= this.resultList.size())
          return 0;
 
-      return resultList.get(resultSetNumber).getRowCount();
+      return this.resultList.get(resultSetNumber).getRowCount();
    }
 
 
@@ -37,16 +43,135 @@ public class DBResult {
       if (result == null)
          return -1;
 
-      resultList.add(result);
+      this.resultList.add(result);
 
-      return resultList.size();
+      return this.resultList.size();
+   }
+
+
+   public BigDecimal getValue(int resultSetNumber, int column, int row, BigDecimal asBigDecimalValue) {
+      return getValue(resultSetNumber, column, row, (BigDecimal)null, asBigDecimalValue);
+   }
+
+
+   public BigDecimal getValue(int resultSetNumber, int column, int row, BigDecimal defaultValue, BigDecimal asBigDecimalValue) {
+      if (!checkResultSetNumber(resultSetNumber))
+         return defaultValue;
+
+      return this.resultList.get(resultSetNumber).getValue(column, row, defaultValue, asBigDecimalValue);
+   }
+
+
+   public BigDecimal getValue(int resultSetNumber, String columnName, int row, BigDecimal asBigDecimalValue) {
+      return getValue(resultSetNumber, columnName, row, (BigDecimal)null, asBigDecimalValue);
+   }
+
+
+   public BigDecimal getValue(int resultSetNumber, String columnName, int row, BigDecimal defaultValue, BigDecimal asBigDecimalValue) {
+      if (!checkResultSetNumber(resultSetNumber))
+         return defaultValue;
+
+      return this.resultList.get(resultSetNumber).getValue(columnName, row, defaultValue, asBigDecimalValue);
+   }
+
+
+   public Boolean getValue(int resultSetNumber, int column, int row, Boolean asBooleanValue) {
+      return getValue(resultSetNumber, column, row, (Boolean)null, asBooleanValue);
+   }
+
+
+   public Boolean getValue(int resultSetNumber, int column, int row, Boolean defaultValue, Boolean asBooleanValue) {
+      if (!checkResultSetNumber(resultSetNumber))
+         return defaultValue;
+
+      return this.resultList.get(resultSetNumber).getValue(column, row, defaultValue, asBooleanValue);
+   }
+
+
+   public Boolean getValue(int resultSetNumber, String columnName, int row, Boolean asBooleanValue) {
+      return getValue(resultSetNumber, columnName, row, (Boolean)null, asBooleanValue);
+   }
+
+
+   public Boolean getValue(int resultSetNumber, String columnName, int row, Boolean defaultValue, Boolean asBooleanValue) {
+      if (!checkResultSetNumber(resultSetNumber))
+         return defaultValue;
+
+      return this.resultList.get(resultSetNumber).getValue(columnName, row, defaultValue, asBooleanValue);
+   }
+
+
+   public Integer getValue(int resultSetNumber, int column, int row, Integer asIntegerValue) {
+      return getValue(resultSetNumber, column, row, (Integer)null, asIntegerValue);
+   }
+
+
+   public Integer getValue(int resultSetNumber, int column, int row, Integer defaultValue, Integer asIntegerValue) {
+      if (!checkResultSetNumber(resultSetNumber))
+         return defaultValue;
+
+      return this.resultList.get(resultSetNumber).getValue(column, row, defaultValue, asIntegerValue);
+   }
+
+
+   public Integer getValue(int resultSetNumber, String columnName, int row, Integer asIntegerValue) {
+      return getValue(resultSetNumber, columnName, row, (Integer)null, asIntegerValue);
+   }
+
+
+   public Integer getValue(int resultSetNumber, String columnName, int row, Integer defaultValue, Integer asIntegerValue) {
+      if (!checkResultSetNumber(resultSetNumber))
+         return defaultValue;
+
+      return this.resultList.get(resultSetNumber).getValue(columnName, row, defaultValue, asIntegerValue);
+   }
+
+
+   public String getValue(int resultSetNumber, String ColumnName, int row) {
+      return getValue(resultSetNumber, ColumnName, row, (String)null, (String)null);
+   }
+
+
+   public String getValue(int resultSetNumber, String ColumnName, int row, String defaultValue) {
+      return getValue(resultSetNumber, ColumnName, row, defaultValue, (String)null);
+   }
+
+
+   public String getValue(int resultSetNumber, String columnName, int row, String defaultValue, String asStringValue) {
+      if (!checkResultSetNumber(resultSetNumber))
+         return defaultValue;
+
+      return this.resultList.get(resultSetNumber).getValue(columnName, row, defaultValue, asStringValue);
+   }
+
+
+   public String getValue(int resultSetNumber, int column, int row) {
+      return getValue(resultSetNumber, column, row, (String)null, (String)null);
+   }
+
+
+   public String getValue(int resultSetNumber, int column, int row, String defaultValue) {
+      return getValue(resultSetNumber, column, row, defaultValue, (String)null);
+   }
+
+
+   public String getValue(int resultSetNumber, int column, int row, String defaultValue, String asStringValue) {
+      if (!checkResultSetNumber(resultSetNumber))
+         return defaultValue;
+
+      return this.resultList.get(resultSetNumber).getValue(column, row, defaultValue, asStringValue);
+   }
+
+
+   private boolean checkResultSetNumber(int resultSetNumber) {
+      return (resultSetNumber >= 0 && resultSetNumber < this.resultList.size());
    }
 
 
    public String toString() {
       StringBuilder sb = new StringBuilder();
 
-      for (DBSingleResult res : resultList)
+      for (DBSingleResult res : this.resultList)
          sb.append(res.toString());
 
       return sb.toString();
--- src/main/java/de/mss/utils/db/DBSingleResult.java
+++ src/main/java/de/mss/utils/db/DBSingleResult.java
@@ -1,6 +1,9 @@
 package de.mss.utils.db;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
+
+import de.mss.utils.Tools;
 
 public class DBSingleResult {
 
@@ -10,14 +13,14 @@ public class DBSingleResult {
 
 
    public DBSingleResult(int resNumber, int columnCount) {
-      columnNames = new DBDataRow(columnCount);
-      rowValues = new ArrayList<>();
+      this.columnNames = new DBDataRow(columnCount);
+      this.rowValues = new ArrayList<>();
       this.resultSetNumber = resNumber;
    }
 
 
    public int addRow() {
-      this.rowValues.add(new DBDataRow(columnNames.getSize()));
+      this.rowValues.add(new DBDataRow(this.columnNames.getSize()));
       return this.rowValues.size();
    }
 
@@ -39,13 +42,111 @@ public class DBSingleResult {
 
 
    public String getValue(int column, int row) {
+      return getValue(column, row, (String)null, (String)null);
+   }
+
+
+   public String getValue(int column, int row, String asStringValue) {
+      return getValue(column, row, (String)null, asStringValue);
+   }
+
+
+   public String getValue(String columnName, int row, String defaultValue, String asStringValue) {
+      return getValue(getColumnIndex(columnName), row, defaultValue, asStringValue);
+   }
+
+
+   public String getValue(int column, int row, String defaultValue, @SuppressWarnings("unused") String asStringValue) {
       if (column < 0 || column >= this.columnNames.getSize())
-         return null;
+         return defaultValue;
 
       if (row < 0 || row >= this.rowValues.size())
-         return null;
+         return defaultValue;
 
       return this.rowValues.get(row).getValue(column);
+   }
+
+
+   public Integer getValue(String columnName, int row, Integer asIntegerValue) {
+      return getValue(getColumnIndex(columnName), row, (Integer)null, asIntegerValue);
+   }
+
+
+   public Integer getValue(String columnName, int row, Integer defaultValue, Integer asIntegerValue) {
+      return getValue(getColumnIndex(columnName), row, defaultValue, asIntegerValue);
+   }
+
+
+   public Integer getValue(int column, int row, Integer asIntegerValue) {
+      return getValue(column, row, (Integer)null, asIntegerValue);
+   }
+
+
+   public Integer getValue(int column, int row, Integer defaultValue, @SuppressWarnings("unused") Integer asIntegerValue) {
+      String s = getValue(column, row, (String)null, (String)null);
+      if (s == null)
+         return defaultValue;
+
+      try {
+         return Integer.valueOf(s);
+      }
+      catch (Exception e) {}
+      return defaultValue;
+   }
+
+
+   public BigDecimal getValue(String columnName, int row, BigDecimal asBigDecimalValue) {
+      return getValue(getColumnIndex(columnName), row, (BigDecimal)null, asBigDecimalValue);
+   }
+
+
+   public BigDecimal getValue(String columnName, int row, BigDecimal defaultValue, BigDecimal asBigDecimalValue) {
+      return getValue(getColumnIndex(columnName), row, defaultValue, asBigDecimalValue);
+   }
+
+
+   public BigDecimal getValue(int column, int row, BigDecimal asBigDecimalValue) {
+      return getValue(column, row, (BigDecimal)null, asBigDecimalValue);
+   }
+
+
+   public BigDecimal getValue(int column, int row, BigDecimal defaultValue, @SuppressWarnings("unused") BigDecimal asBigDecimalValue) {
+      String s = getValue(column, row, (String)null, (String)null);
+      if (s == null)
+         return defaultValue;
+
+      try {
+         return new BigDecimal(s);
+      }
+      catch (Exception e) {}
+      return defaultValue;
+   }
+
+
+   public Boolean getValue(String columnName, int row, Boolean asBooleanValue) {
+      return getValue(getColumnIndex(columnName), row, (Boolean)null, asBooleanValue);
+   }
+
+
+   public Boolean getValue(String columnName, int row, Boolean defaultValue, Boolean asBooleanValue) {
+      return getValue(getColumnIndex(columnName), row, defaultValue, asBooleanValue);
+   }
+
+
+   public Boolean getValue(int column, int row, Boolean asBooleanValue) {
+      return getValue(column, row, (Boolean)null, asBooleanValue);
+   }
+
+
+   public Boolean getValue(int column, int row, Boolean defaultValue, @SuppressWarnings("unused") Boolean asBooleanValue) {
+      String s = getValue(column, row, (String)null, (String)null);
+      if (s == null)
+         return defaultValue;
+
+      if (Tools.isTrue(s))
+         return Boolean.TRUE;
+
+      return Boolean.FALSE;
    }
 
 
@@ -75,10 +176,10 @@ public class DBSingleResult {
 
 
    public void setColumnName(String columnName, int col) {
-      if (col < 0 || col >= columnNames.getSize())
+      if (col < 0 || col >= this.columnNames.getSize())
          return;
 
-      columnNames.setValue(columnName, col);
+      this.columnNames.setValue(columnName, col);
    }
 
 
