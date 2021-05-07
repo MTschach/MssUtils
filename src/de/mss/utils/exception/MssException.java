d3cb0a686926ad740f5b66767ef17722a1bfa405
michael <michael@MT-1>
2021-05-07 16:05:06 +0200

    - errorCode and errorText added

--- src/de/mss/utils/exception/MssException.java
+++ src/de/mss/utils/exception/MssException.java
@@ -15,6 +15,11 @@ public class MssException extends Exception {
    }
 
 
+   public MssException(Error ec, String msg) {
+      init(ec, 0, msg, null);
+   }
+
+
    public MssException(Error ec, Throwable t) {
       init(ec, 0, null, t);
    }
@@ -25,13 +30,13 @@ public class MssException extends Exception {
    }
 
 
-   public MssException(Error ec, String msg) {
-      init(ec, 0, msg, null);
+   public MssException(int code) {
+      init(null, code, null, null);
    }
 
 
-   public MssException(int code) {
-      init(null, code, null, null);
+   public MssException(int code, String msg) {
+      init(null, code, msg, null);
    }
 
 
@@ -45,59 +50,6 @@ public class MssException extends Exception {
    }
 
 
-   public MssException(int code, String msg) {
-      init(null, code, msg, null);
-   }
-
-
-   protected void init(Error e, int ec, String msg, Throwable t) {
-      if (e != null)
-         this.error = e;
-
-      this.altErrorCode = ec;
-      this.altErrorText = msg;
-
-      if (t != null)
-         this.initCause(t);
-   }
-
-
-   @Override
-   public String toString() {
-      StringBuilder sb = new StringBuilder();
-      if (!ErrorCodes.NO_ERROR.equals(this.error))
-         sb
-               .append(
-                     "Error : "
-                           + this.error.getErrorCode()
-                           + "("
-                           + (this.altErrorText == null ? this.error.getErrorText() : this.altErrorText)
-                           + ")");
-      else
-         sb.append("Error : " + this.altErrorCode + "(" + this.altErrorText + ")");
-
-      sb.append(getStackTrace(this));
-
-      return sb.toString();
-   }
-
-
-   protected String getStackTrace(Exception e) {
-      StringBuilder sb = new StringBuilder();
-
-      for (StackTraceElement trace : e.getStackTrace()) {
-         sb.append(trace.toString() + "\n");
-      }
-
-      return sb.toString();
-   }
-
-
-   public Error getError() {
-      return this.error;
-   }
-
-
    public int getAltErrorCode() {
       return this.altErrorCode;
    }
@@ -107,4 +59,64 @@ public class MssException extends Exception {
       return this.altErrorText;
    }
 
+
+   public Error getError() {
+      return this.error;
+   }
+
+
+   public Integer getErrorCode() {
+      return this.altErrorCode != 0 ? Integer.valueOf(this.altErrorCode) : Integer.valueOf(this.error.getErrorCode());
+   }
+
+
+   public String getErrorText() {
+      return this.altErrorText != null ? this.altErrorText : this.error.getErrorText();
+   }
+
+
+   protected String getStackTrace(Exception e) {
+      final StringBuilder sb = new StringBuilder();
+
+      for (final StackTraceElement trace : e.getStackTrace()) {
+         sb.append(trace.toString() + "\n");
+      }
+
+      return sb.toString();
+   }
+
+
+   protected void init(Error e, int ec, String msg, Throwable t) {
+      if (e != null) {
+         this.error = e;
+      }
+
+      this.altErrorCode = ec;
+      this.altErrorText = msg;
+
+      if (t != null) {
+         this.initCause(t);
+      }
+   }
+
+
+   @Override
+   public String toString() {
+      final StringBuilder sb = new StringBuilder();
+      if (!ErrorCodes.NO_ERROR.equals(this.error)) {
+         sb
+               .append(
+                     "Error : "
+                           + this.error.getErrorCode()
+                           + "("
+                           + (this.altErrorText == null ? this.error.getErrorText() : this.altErrorText)
+                           + ")");
+      } else {
+         sb.append("Error : " + this.altErrorCode + "(" + this.altErrorText + ")");
+      }
+
+      sb.append(getStackTrace(this));
+
+      return sb.toString();
+   }
 }
--- test/de/mss/utils/exception/MssExceptionTest.java
+++ test/de/mss/utils/exception/MssExceptionTest.java
@@ -6,16 +6,72 @@ import junit.framework.TestCase;
 
 public class MssExceptionTest extends TestCase {
 
+   protected void checkException(MssException e, Error expError, boolean throwable, String expErrorText, int expErrorCode) {
+      assertEquals("ErrorCode", expError.getErrorCode(), e.getError().getErrorCode());
+      assertEquals("ErrorText", expError.getErrorText(), e.getError().getErrorText());
+
+      assertEquals("Alt ErrorCode", expErrorCode, e.getAltErrorCode());
+
+      if (expErrorText != null) {
+         assertEquals("Alt Error text", expErrorText, e.getAltErrorText());
+      } else {
+         assertNull("Alt ErrorText", e.getAltErrorText());
+      }
+
+      if (throwable) {
+         assertNotNull("Cause not null", e.getCause());
+      } else {
+         assertNull("Cause is null", e.getCause());
+      }
+
+      final StringBuilder expMsg = new StringBuilder("Error : ");
+      if (expError != ErrorCodes.NO_ERROR) {
+         expMsg.append(expError.getErrorCode() + "(" + (expErrorText == null ? expError.getErrorText() : expErrorText) + ")");
+      } else {
+         expMsg.append(expErrorCode + "(" + expErrorText + ")");
+      }
+
+      final String msg = e.toString();
+
+      assertTrue("toString", msg.startsWith(expMsg.toString()));
+
+   }
+
+
+   @Test
+   public void testAltErrorCode() {
+      final MssException e = new MssException(4);
+      checkException(e, ErrorCodes.NO_ERROR, false, null, 4);
+   }
+
+
+   @Test
+   public void testCodeText() {
+      final MssException e = new MssException(5, "an error");
+      checkException(e, ErrorCodes.NO_ERROR, false, "an error", 5);
+   }
+
+
    @Test
    public void testErrorCode() {
-      MssException e = new MssException(new Error(ErrorCodes.ERROR_NOT_INSTANCABLE.getErrorCode(), ErrorCodes.ERROR_NOT_INSTANCABLE.getErrorText()));
+      final MssException e = new MssException(
+            new Error(ErrorCodes.ERROR_NOT_INSTANCABLE.getErrorCode(), ErrorCodes.ERROR_NOT_INSTANCABLE.getErrorText()));
       checkException(e, ErrorCodes.ERROR_NOT_INSTANCABLE, false, null, 0);
    }
 
 
    @Test
+   public void testErrorCodeMessage() {
+      final MssException e = new MssException(
+            new Error(ErrorCodes.ERROR_NOT_INSTANCABLE.getErrorCode(), ErrorCodes.ERROR_NOT_INSTANCABLE.getErrorText()),
+            "own errortext");
+      checkException(e, ErrorCodes.ERROR_NOT_INSTANCABLE, false, "own errortext", 0);
+   }
+
+
+   @Test
    public void testErrorCodeThrowable() {
-      MssException e = new MssException(
+      final MssException e = new MssException(
             new Error(ErrorCodes.ERROR_NOT_INSTANCABLE.getErrorCode(), ErrorCodes.ERROR_NOT_INSTANCABLE.getErrorText()),
             new Throwable());
       checkException(e, ErrorCodes.ERROR_NOT_INSTANCABLE, true, null, 0);
@@ -24,7 +80,7 @@ public class MssExceptionTest extends TestCase {
 
    @Test
    public void testErrorCodeThrowableMsg() {
-      MssException e = new MssException(
+      final MssException e = new MssException(
             new Error(ErrorCodes.ERROR_NOT_INSTANCABLE.getErrorCode(), ErrorCodes.ERROR_NOT_INSTANCABLE.getErrorText()),
             new Throwable(),
             "own errortext");
@@ -33,69 +89,29 @@ public class MssExceptionTest extends TestCase {
 
 
    @Test
-   public void testErrorCodeMessage() {
-      MssException e = new MssException(
-            new Error(ErrorCodes.ERROR_NOT_INSTANCABLE.getErrorCode(), ErrorCodes.ERROR_NOT_INSTANCABLE.getErrorText()),
-            "own errortext");
-      checkException(e, ErrorCodes.ERROR_NOT_INSTANCABLE, false, "own errortext", 0);
+   public void testGetErrorCode() {
+      assertEquals(Integer.valueOf(2), new MssException(ErrorCodes.ERROR_INVALID_PARAM).getErrorCode());
+      assertEquals(Integer.valueOf(3), new MssException(3).getErrorCode());
    }
 
 
    @Test
-   public void testAltErrorCode() {
-      MssException e = new MssException(4);
-      checkException(e, ErrorCodes.NO_ERROR, false, null, 4);
+   public void testGetErrorText() {
+      assertEquals("invalid parameter", new MssException(ErrorCodes.ERROR_INVALID_PARAM).getErrorText());
+      assertEquals("an error", new MssException(ErrorCodes.ERROR_INVALID_PARAM, "an error").getErrorText());
    }
 
 
    @Test
    public void testThrowable() {
-      MssException e = new MssException(new Throwable());
+      final MssException e = new MssException(new Throwable());
       checkException(e, ErrorCodes.NO_ERROR, true, null, 0);
    }
 
 
    @Test
    public void testThrowableMsg() {
-      MssException e = new MssException(new Throwable(), "own errortext");
+      final MssException e = new MssException(new Throwable(), "own errortext");
       checkException(e, ErrorCodes.NO_ERROR, true, "own errortext", 0);
-   }
-
-
-   @Test
-   public void testCodeText() {
-      MssException e = new MssException(5, "an error");
-      checkException(e, ErrorCodes.NO_ERROR, false, "an error", 5);
-   }
-
-
-   protected void checkException(MssException e, Error expError, boolean throwable, String expErrorText, int expErrorCode) {
-      assertEquals("ErrorCode", expError.getErrorCode(), e.getError().getErrorCode());
-      assertEquals("ErrorText", expError.getErrorText(), e.getError().getErrorText());
-
-      assertEquals("Alt ErrorCode", expErrorCode, e.getAltErrorCode());
-
-      if (expErrorText != null)
-         assertEquals("Alt Error text", expErrorText, e.getAltErrorText());
-      else
-         assertNull("Alt ErrorText", e.getAltErrorText());
-
-      if (throwable)
-         assertNotNull("Cause not null", e.getCause());
-      else
-         assertNull("Cause is null", e.getCause());
-
-      StringBuilder expMsg = new StringBuilder("Error : ");
-      if (expError != ErrorCodes.NO_ERROR) {
-         expMsg.append(expError.getErrorCode() + "(" + (expErrorText == null ? expError.getErrorText() : expErrorText) + ")");
-      }
-      else {
-         expMsg.append(expErrorCode + "(" + expErrorText + ")");
-      }
-
-      String msg = e.toString();
-
-      assertTrue("toString", msg.startsWith(expMsg.toString()));
-
    }
 }
